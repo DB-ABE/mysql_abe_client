13fbbf92d19d6da771b8c0200cd35cd50e3df40e
hpq <2080240649@qq.com>
2023-11-13 17:46:10 +0800

    新增：单元测试（abe_crypto:verify）

--- unittest/abe_crypto_unittest.cpp
+++ unittest/abe_crypto_unittest.cpp
@@ -1,8 +1,15 @@
 #include "src/abe_crypto.h"
+#include "src/my_utils/base64.h"
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "gmock-global.h"
+#include <openssl/sha.h>
+#include <openssl/rsa.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include "openssl/crypto.h"
 #include <fstream>
+#include <iostream>
 #include <string>
 using std::string;
 
@@ -26,6 +33,10 @@ protected:
 
 	string abe_key_path = base_dir + "abe/abe_key";
 	string abe_pp_path = base_dir + "abe/abe_pp";
+
+	//签名验证用：
+	string db_sk_path = base_dir + "prikey/db.pem";
+	string kms_sk_path = base_dir + "prikey/kms.pem";
 };
 
 TEST_F(cryptoTest, cryptoInitSuccess1){
@@ -144,7 +155,7 @@ TEST_F(cryptoTest, cryptoImportDbCert){
 TEST_F(cryptoTest, cryptoImportKmsCert){
 	abe_crypto crypto("testabe@%");
 	bool res;
-	res = crypto.import_kms_cert(db_cert_path);
+	res = crypto.import_kms_cert(kms_cert_path);
 	EXPECT_EQ(true, res);
 	string new_path = base_dir + "non-exist-path";
     res = crypto.import_kms_cert(new_path);
@@ -200,4 +211,98 @@ TEST_F(cryptoTest, cryptoDecFail){
 		EXPECT_EQ(true, res);//属性（attr1 | attr2）不符合策略
 		EXPECT_EQ("can't decrypt.", pt);//解密失败返回值
 	}
+}
+
+// 签名 use private key
+bool my_RSA_Sign(const std::string strPemFileName, std::string strData,
+             unsigned char *pEncode, unsigned int &outlen)
+{
+    // 读取rsa私钥文件，导入私钥
+    FILE *hPriKeyFile = fopen(strPemFileName.c_str(), "rb");
+    if (hPriKeyFile == NULL)
+    {
+        return false;
+    }
+    RSA *pRSAPriKey = RSA_new();
+    if (PEM_read_RSAPrivateKey(hPriKeyFile, &pRSAPriKey, 0, 0) == NULL)
+    {
+		fclose(hPriKeyFile);
+        return false;
+    }
+	fclose(hPriKeyFile);
+
+    // 签名
+    unsigned char digest[NID_sha512];
+    SHA512((unsigned char *)strData.c_str(), strData.length(), digest);
+    int ret = RSA_sign(NID_sha512, (const unsigned char *)digest, SHA512_DIGEST_LENGTH,
+                       pEncode, &outlen, pRSAPriKey);
+    // 释放资源
+    RSA_free(pRSAPriKey);
+    
+    return ret == 1 ? true: false;
+}
+
+TEST_F(cryptoTest, cryptoVerifyDbSig){
+	abe_crypto crypto("testabe@%");
+	bool res = crypto.import_db_cert(db_cert_path);//验签需要证书
+
+	EXPECT_EQ(true, res);
+	string msg_raw = "hello";//原始信息
+	unsigned char RSA_sign_buf[257];
+	unsigned int sign_len;
+	res = my_RSA_Sign(db_sk_path, msg_raw, RSA_sign_buf, sign_len);
+	if(res){
+		unsigned char * sig_b64 = (unsigned char*)malloc(base64_utils::b64_enc_len(sign_len));
+		size_t sig_b64_length = base64_utils::b64_encode((char *)RSA_sign_buf, sign_len, (char*)sig_b64);
+		string sig_b64_str((char*)sig_b64, sig_b64_length);
+		bool res2 = crypto.verify_db_sig(msg_raw, sig_b64_str);
+		EXPECT_EQ(res2, true);
+
+		//使用错误的证书
+		res2 = crypto.import_db_cert(kms_cert_path);//验签需要证书
+		EXPECT_EQ(true, res2);
+		res2 = crypto.verify_db_sig(msg_raw, sig_b64_str);
+		EXPECT_EQ(false, res2);
+
+		free(sig_b64);
+	}else{
+		std::cout << "生成测试用签名失败" << std::endl;
+	}
+}
+
+TEST_F(cryptoTest, cryptoVerifyKmsSig){
+	abe_crypto crypto("testabe@%");
+	bool res = crypto.import_kms_cert(kms_cert_path);//验签需要证书
+
+	EXPECT_EQ(true, res);
+	string msg_raw = "hello";//原始信息
+	size_t msg_len = msg_raw.length();
+	unsigned char * msg_b64 = (unsigned char*)malloc(base64_utils::b64_enc_len(msg_len));
+	size_t msg_b64_length = base64_utils::b64_encode(msg_raw.c_str(), msg_len, (char*)msg_b64);
+	string msg_b64_str((char*)msg_b64, msg_b64_length);
+
+	unsigned char RSA_sign_buf[257];
+	unsigned int sign_len;
+	res = my_RSA_Sign(kms_sk_path, msg_raw, RSA_sign_buf, sign_len);
+	if(res){
+		unsigned char * sig_b64 = (unsigned char*)malloc(base64_utils::b64_enc_len(sign_len));
+		size_t sig_b64_length = base64_utils::b64_encode((char *)RSA_sign_buf, sign_len, (char*)sig_b64);
+		string sig_b64_str((char*)sig_b64, sig_b64_length);
+		
+
+		bool res2 = crypto.verify_kms_sig(msg_b64_str, sig_b64_str);
+		EXPECT_EQ(res2, true);
+
+		//使用错误的证书
+		res2 = crypto.import_kms_cert(db_cert_path);//验签需要证书
+		EXPECT_EQ(true, res2);
+		res2 = crypto.verify_kms_sig(msg_b64_str, sig_b64_str);
+		EXPECT_EQ(false, res2);
+		free(sig_b64);
+
+	}else{
+		std::cout << "生成测试用签名失败" << std::endl;
+	}
+	
+	free(msg_b64);
 }
